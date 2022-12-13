ce24e217c168b445481b72d048fbb1052f6e7830
certihimo <kalidahm3d@gmail.com>
2022-12-13 16:13:08 +0300

    Completed

--- index.js
+++ index.js
@@ -1,88 +1,78 @@
-const ul = document.getElementById("film")
-let liElement;
-let posterElement = document.getElementById("poster")
-let titleElement = document.getElementById("title")
-let descElement = document.getElementById("desc")
-let runtimeElement = document.getElementById("runtime")
-let capacityElement = document.getElementById("capacity")
-let ticketElement = document.getElementById("tickets")
-let btnElement = document.getElementById("btn")
-
-function getFirstMovie(){
-    console.log("first")
-    fetch("http://localhost:3000/films/1")
-    .then(function(response){
-        return response.json()
-    }).then(function(data){
-       posterElement.src= data.poster
-       titleElement.innerHTML = data.title
-       descElement.innerHTML = data.description
-       runtimeElement.innerHTML = data.runtime
-       capacityElement.innerHTML = data.capacity
-       ticketElement.innerHTML = data.capacity-data.tickets_sold>0?data.capacity-data.tickets_sold:"Sold out"
-       if(data.capacity-data.tickets_sold <= 0){
-        btnElement.style.display = "none";
-       }
-       btnElement.setAttribute("onclick", `buyTicket(${data.tickets_sold},${data.id})`)
-    })
+function fetchFilms() {
+    fetch('http://localhost:3000/films')
+      .then(res => res.json())
+      .then(data => renderFilms(data));
 }
 
-function getMovieTitles(){
-    fetch("http://localhost:3000/films")
-    .then(function(response){
-        return response.json()
-    }).then(function(data){
-        data.forEach(movie => {
-           liElement = document.createElement("li")
-           ul.appendChild(liElement) 
-           liElement.innerHTML = movie.title
-           liElement.setAttribute("onclick", `getMovieDetails(${movie.id})`)
-        });
-    })
-}
-function getMovieDetails(id){
-    console.log("all")
-    location.replace("http://127.0.0.1:5500/index.html?id="+id);
-   
-}
-function buyTicket(ticket_sold, id){
-    fetch(`http://localhost:3000/films/${id}`, {
-            method: 'PATCH',
-            body: JSON.stringify({
-              tickets_sold: ticket_sold+1,
-            }),
-            headers: {
-              'Content-type': 'application/json; charset=UTF-8',
-            },
-          })
-            .then((response) => response.json())
-            .then((json) => console.log(json));
-          
-     }
 
-getMovieTitles()
-function getMovies(){
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const urlId = urlParams.get('id')
-    fetch("http://localhost:3000/films/"+urlId)
-    .then(function(response){
-        return response.json()
-    }).then(function(data){
-        posterElement.src= data.poster
-        titleElement.innerHTML = data.title
-        descElement.innerHTML = data.description
-        runtimeElement.innerHTML = data.runtime
-        capacityElement.innerHTML = data.capacity
-        ticketElement.innerHTML = data.capacity-data.tickets_sold>0?data.capacity-data.tickets_sold:"Sold out"
-        if(data.capacity-data.tickets_sold <= 0){
-            btnElement.style.display = "none";
-           }
-        btnElement.setAttribute("onclick", `buyTicket(${data.tickets_sold},${data.id})`)
-    })
-}
-getMovies()
-if(window.location.href == "http://127.0.0.1:5500/index.html" ||window.location.href == "http://127.0.0.1:5500/"){
-    getFirstMovie()
-}
-console.log(window.location.href)
+
+function renderFilms(data) {
+    const div = document.getElementById('card');
+    const ul = document.getElementById('films');
+    
+
+
+    data.forEach(movie => {
+      const li = document.createElement('li');
+      li.classList.add('pointer', 'bold-italic-text');
+      li.innerHTML = movie.title;
+
+
+      const filmCard = document.createElement("div");
+      filmCard.classList.add('film-card');
+      filmCard.innerHTML = `
+        <img src="${movie.poster}" height=500px width=300px/>
+        <h2 class="bold-text">${movie.title}</h2>
+        <p class="bold-text">${movie.description}</p>
+        <p><span class="highlight bold-text">Runtime: ${movie.runtime}</span></p>
+        <p><span class="highlight bold-text">Showtime: ${movie.showtime}</span></p>
+      `;
+      // Create a new <p> element to display the number of available tickets
+      const tickets = document.createElement("p");
+      tickets.classList.add("bold-italic-text")
+      tickets.innerHTML = `Available tickets: ${(movie.capacity) - (movie.tickets_sold)}`;
+      // Append the new <p> element to the filmCard
+      filmCard.appendChild(tickets);
+
+
+
+
+
+      const btn = document.createElement("button");
+      btn.textContent = "Buy ticket";
+      // Add an event listener to the button to decrement the number of tickets when clicked
+      btn.addEventListener('click', () => {
+        // Check if the number of available tickets is 0
+        if (parseInt(tickets.innerText.split(': ')[1]) === 0) {
+          // If it is, show an alert
+          alert("Ticket Sold Out");
+        } else {
+          // Otherwise, decrement the number of available tickets by 1
+          tickets.innerText = `Available tickets: ${parseInt(tickets.innerText.split(': ')[1]) - 1}`;
+        }
+      });
+      // Append the button to the filmCard
+      filmCard.appendChild(btn);
+
+
+
+      li.addEventListener('click', () => {
+        div.innerText=""
+        div.appendChild(filmCard);
+         // Check if the filmCard has the "active" class
+         if (!filmCard.classList.contains('active')) {
+          // If it does not, add the "active" class and append the filmCard to the li element
+          filmCard.classList.add('active');  
+          div.appendChild(filmCard);
+
+        }
+        /*else if (filmCard.classList.contains('active')){
+          filmCard.classList.remove('active');
+          div.appendChild(filmCard);
+        }*/
+      
+      });
+      ul.appendChild(li);
+    });
+  }
+  fetchFilms();
